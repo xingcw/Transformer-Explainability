51006bc73795eb580e0ec6b5ab08ed65218673f7
chunwei <xingcw61@gmail.com>
2023-03-14 15:08:33 +0100

    remove requirements from setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 import setuptools
-from pip._internal.req import parse_requirements
-
-
-install_reqs = parse_requirements("requirements.txt", session="test")
-reqs = [str(ir.requirement) for ir in install_reqs]
 
 __version__ = '0.0.1'
 
@@ -13,5 +8,4 @@ setuptools.setup(
     name='trans_exp',
     version=__version__,
     packages=pkgs,
-    install_requires=reqs
 )
